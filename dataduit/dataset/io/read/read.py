ec86d5739a9b3cbe66dd210703d68e5d9e62cb9b
Jack Burdick <jackbburdick@gmail.com>
2020-01-08 20:56:09 -0800

    begin pandas dev
    .. out of time tonight :(

--- dataduit/dataset/io/read/read.py
+++ dataduit/dataset/io/read/read.py
@@ -2,13 +2,58 @@ from dataduit.dataset.io.read.location.local.read import read_local
 from dataduit.dataset.io.read.location.memory.read import read_memory
 
 
-def read(config_dict):
+def read(config_dict, obj=None):
+
+    # TODO: this logic block will need to be rewritten to something a little more understandable
+    ACCEPTED_TYPES = ["records", "pandas", "numpy", "tfd"]
+    try:
+        stage = config_dict["read"]["from_stage"]
+    except KeyError:
+        try:
+            meta_from = config_dict["meta"]["in"]["from"]
+        except KeyError:
+            raise ValueError(
+                f"read:from_stage not specified and meta:in:from not specified"
+            )
+
+        if meta_from == "memory":
+            stage = "memory"
+
+            try:
+                data_read_type = config_dict["meta"]["in"]["type"].lower()
+            except KeyError:
+                raise KeyError(
+                    f"meta:in:type: currently not specified, please select one of {ACCEPTED_TYPES}"
+                )
+
+            if not data_read_type in ACCEPTED_TYPES:
+                raise ValueError(
+                    f"{data_read_type} currently not supported, please select one of {ACCEPTED_TYPES}"
+                )
+        else:
+            raise KeyError(
+                f"meta:in:from is specified as {meta_from} but only 'memory' is currently supported"
+            )
 
     # TODO: this needs to be corrected
-    if config_dict["read"]["from_stage"] == "records":
+    if stage == "records":
         raise NotImplementedError("reading from records not supported yet")
-    elif config_dict["read"]["from_stage"] == "preprocessed":
+    elif stage == "preprocessed":
         datasets = read_local(config_dict)
+    elif stage == "memory":
+
+        # TODO: push block down a level
+        if data_read_type == "pandas":
+            raise NotImplementedError(f"{data_read_type} currently not supported")
+        elif data_read_type == "records":
+            raise NotImplementedError(f"{data_read_type} currently not supported")
+        elif data_read_type == "tfd":
+            raise NotImplementedError(f"{data_read_type} currently not supported")
+        elif data_read_type == "numpy":
+            raise NotImplementedError(f"{data_read_type} currently not supported")
+        else:
+            raise NotImplementedError(f"{data_read_type} currently not supported")
+
     else:
         raise NotImplementedError(
             f"reading from {config_dict['read']['from']} not supported yet"
---
+++ pandas_dev.py
@@ -0,0 +1,65 @@
+conf_dict = {
+    "meta": {
+        "name": "albalone",
+        # "root_location": "../datasets",  # relative path
+        "logging": {"log_stream_level": "INFO"},
+        "in": {
+            "from": "memory",  # memory, local, online
+            "type": "pandas",  # records, pandas, numpy, tfd, TODO:include popular websites
+        },
+    },  # The name must be valid if using tf
+    "read": {  # create_ds should call obtain()
+        # "from_stage": "memory",  # ["records","preprocessed"]
+        # "split_defaults": False # this would use the default splits specified (by TF datasets)
+        "split_percents": [75, 15, 10],  # currently only support percents
+        "split_names": ["train", "val", "test"],
+        # if "out" is missing in each of "identifier", "datatype", "shape", and "missing", in is used
+        "iterate": {
+            "return_type": "tuple",  # ["tuple", or dict]
+            "schema": {  # the order defined below is the order in which items will be returned. only simple logic allowed
+                # TODO: we need to ensure there are no name collisions here/across each highlevel grouping
+                "x": {
+                    "length": {
+                        "datatype": {
+                            "in": {
+                                "type": "FixedLenFeature",
+                                "options": {"dtype": "int64", "shape": 1},
+                            },
+                            "out": {},
+                        },
+                        "special": "decode",
+                    },
+                    "diameter": {
+                        "datatype": {
+                            "in": {
+                                "type": "FixedLenFeature",
+                                "options": {"dtype": "int64", "shape": 1},
+                            },
+                            "out": {},
+                        },
+                        "special": "decode",
+                    },
+                },
+                "y": {
+                    "rings": {
+                        "datatype": {
+                            "in": {
+                                "type": "FixedLenFeature",
+                                "options": {"dtype": "int64", "shape": 1},
+                            },
+                            "out": {},
+                        }
+                    },
+                    # "missing": {"skip_row": True, "log": True, "create_mask": True},
+                    # "statistics": {<if anything is known.. max/min val, median, basic stats?>}}
+                },
+            },
+        },
+    },
+}
+# missing:
+#             skip: ['True', 'False']
+#             log:
+#                 location: "<_path_>"
+#             fill_with: ['nan', 'mean', 'median', ...]
+#             mask: ['True', 'False']
